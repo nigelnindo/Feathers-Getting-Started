d1f4cc458e45bb477127ddc10210e5130572424c
Nigel Nindo <nigelnindo@gmail.com>
2016-08-01 01:08:32 +0300

    Final working implementation of REST API

--- src/services/message/hooks/process.js
+++ src/services/message/hooks/process.js
@@ -5,21 +5,28 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/hooks/readme.html
 
-//const defaults = {};
+const defaults = {};
 
-module.exports = function() {
+/**
+ * When making requests, make sure to define Content-Type Header to avoid strange errors
+ */
+
+module.exports = function(options) {
   //options = Object.assign({}, defaults, options);
 
   return function(hook) {
     // The authenticated user
+    //console.info(hook);
+
     const user = hook.params.user; // note the difference between hook.data & hook.params
 
     // Actual message text
-    const text = hook.data.text
+
+    const text = hook.data.text.substring(0,400).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
       // Messages can't be longer than 400 characters
-      .substring(0,400)
+
       // Basic HTML escaping
-      .replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+
 
     // Override the original data
     hook.data = {
